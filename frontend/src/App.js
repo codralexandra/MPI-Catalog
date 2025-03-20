cbb9ee38168f01f26cdf98c524ef7b7072e6d1b6
ioana manghiuc <147768804+ioana-manghiuc@users.noreply.github.com>
2025-03-20 19:28:57 +0200

    SCRUM-40 Implemented basic login form.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import Login from './Components/Login';
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 
 function App() {
   return (
-    <Login/>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,8 +1,60 @@
 import '../Stylesheets/Login.css';
+import { useRef, useState, useEffect } from "react";
 
 function Login() {
+
+    const loginRef = useRef();
+
+    const [login, setLogin] = useState("");
+    const [pwd, setPwd] = useState("");
+
+    useEffect(() => {
+        loginRef.current.focus();
+    }, []);
+
+    useEffect(() =>{
+        setLogin(login);
+        console.log("Username or email", login);
+        setPwd(pwd);
+        console.log("Password", pwd);
+    },[login,pwd])
+
+
+    const handleLogin = (e) => {
+        // Here data will be sent to backend for confirmation of credentials.
+    }
+
     return (
-      <div className="text">login page here</div>
+        <section className="login-container">
+            <h1>LOG IN PAGE</h1>
+            <form onSubmit={handleLogin} className="login-form">
+                <div className="input-group">
+                    <label htmlFor="login">Username or Email:</label>
+                    <input
+                        type="text"
+                        id="login"
+                        ref={loginRef}
+                        autoComplete="off"
+                        onChange={(e) => setLogin(e.target.value)}
+                        value={login}
+                        required
+                    />
+                </div>
+
+                <div className="input-group">
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPwd(e.target.value)}
+                        value={pwd}
+                        required
+                    />
+                </div>
+
+                <button>Log In</button>
+            </form>
+        </section>
     );
   }
   
