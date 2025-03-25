8b6bda34d24685ab96571e816f4c4bbbeb8797d9
RobyDoge <robert-ionut.popa@student.unitbv.ro>
2025-03-25 13:20:55 +0200

    SCRUM-109 MOved User Routes in Separated File

---
+++ Backend/User/routes.py
@@ -0,0 +1,48 @@
+from flask import Blueprint
+from User.functionality import User
+
+user_bp = Blueprint('user', __name__)
+
+
+"""
+/login:
+    - Description: Handles user login.
+    - Request Body: Expects 'login' and 'pwd' parameters.
+    - Response: Returns the user ID and role if found in the database, or an error message and code if not found.
+"""
+@user_bp.route('/login', methods=['POST'])
+def login():
+    return User.login()
+
+
+"""
+/register:
+    - Description: Handles user registration.
+    - Request Body: Expects 'username', 'role' and 'pwd' parameters.
+    - Response: Returns a success message if registration is successful, or an error message if registration fails.
+"""
+@user_bp.route('/register', methods=['POST'])
+def register():
+    return User.register()
+
+
+"""
+/reset-password:
+    - Description: Handles password reset.
+    - Request Body: Expects 'login', 'new_pwd' and 'old_pwd' parameters.
+    - Response: Returns a success message if reset is successful, or an error message if the user is not found.
+"""
+@user_bp.route('/reset-password', methods=['POST'])
+def reset_password():
+    return User.reset_password()
+
+
+"""
+/delete-user:
+    - Description: Handles user deletion (for register testing).
+    - Request Body: Expects 'login' parameter.
+    - Response: Returns a success message if delete is successful, or an error message if the user is not found.
+"""
+@user_bp.route('/delete-user', methods=['POST'])
+def delete_user():
+    return User.delete_user()
--- Backend/main.py
+++ Backend/main.py
@@ -2,10 +2,15 @@ import sys
 import os
 # Add the sub-folders to the system path
 sys.path.append(os.path.join(os.path.dirname(__file__), 'User'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'Assigment'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'Teacher'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'Student'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'Coruse'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'Grade'))
 
-from flask import Flask, request
+from flask import Flask
 from flask_cors import CORS
-from User.functionality import User
+from User.routes import user_bp
 
 
 name = 'Gradebook'
@@ -13,51 +18,12 @@ app = Flask(name)
 CORS(app, origins="http://localhost:3000")
 
 
+app.register_blueprint(user_bp)
+
+
 @app.route('/')
 def index():
     return 'Welcome to the Gradebook!'
-
-
-"""
-/login:
-    - Description: Handles user login.
-    - Request Body: Expects 'login' and 'pwd' parameters.
-    - Response: Returns the user ID and role if found in the database, or an error message and code if not found.
-"""
-@app.route('/login', methods=['POST'])
-def login():
-    return User.login()
-    
-    
-"""
-/register:
-    - Description: Handles user registration.
-    - Request Body: Expects 'username', 'role' and 'pwd' parameters.
-    - Response: Returns a success message if registration is successful, or an error message if registration fails.
-"""
-@app.route('/register', methods=['POST'])
-def register():
-    return User.register()
-
-"""
-/reset_password:
-    - Description: Handles password reset.
-    - Request Body: Expects 'login', 'new_pwd' and 'old_pwd' parameters.
-    - Response: Returns a success message if reset is successful, or an error message if the user is not found.
-"""
-@app.route('/reset-password', methods=['POST'])
-def reset_password():
-    return User.reset_password()
-
-"""
-/delete_user:
-    - Description: Handles user deletion (for register testing).
-    - Request Body: Expects 'login' parameter.
-    - Response: Returns a success message if delete is successful, or an error message if the user is not found.
-"""
-@app.route('/delete-user', methods=['POST'])
-def delete_user():
-    return User.delete_user()
 
 if __name__ == '__main__':
     app.run(debug=True, port='5000', host='0.0.0.0')
