d4651e68167488311c673d1333c45a64e9b2cd31
RobyDoge <robert-ionut.popa@student.unitbv.ro>
2025-03-25 14:35:34 +0200

    SCRUM-109 Renamed Teacher Route

--- Backend/Course/teacher_routes.py
+++ Backend/Course/teacher_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from Course.functionality import Course
 
-course_bp = Blueprint('course', __name__, url_prefix='/course')
+teacher_course_bp = Blueprint('course', __name__, url_prefix='/course/teacher')
 
 """
 /get-one:
@@ -9,7 +9,7 @@ course_bp = Blueprint('course', __name__, url_prefix='/course')
     - Request Body: Expects 'course_id'.
     - Response: Returns a course if found, or an error message if no course is found.
 """
-@course_bp.route('/get-one', methods=['POST'])
+@teacher_course_bp.route('/get-one', methods=['POST'])
 def get_one():
     return Course.get_one()
 
@@ -19,7 +19,7 @@ def get_one():
     - Request Body: Expects 'teacher_id'.
     - Response: Returns a list of courses if found, or an error message if no courses are found.
 """
-@course_bp.route('/get-all', methods=['POST'])
+@teacher_course_bp.route('/get-all', methods=['POST'])
 def get_all():
     return Course.get_all()
 
@@ -29,7 +29,7 @@ def get_all():
     - Request Body: Expects 'teacher_id' and 'course_name'.
     - Response: Returns a success message if addition is successful, or an error message if addition fails.
 """
-@course_bp.route('/add', methods=['POST'])
+@teacher_course_bp.route('/add', methods=['POST'])
 def add():
     return Course.add()
 
