acd6e39e85b08d1ac44f78207d5ffc65e908c0da
codralexandra <ioana.codreanu@student.unitbv.ro>
2025-03-29 18:48:08 +0200

    SCRUM-96 Assignment Tests

--- Backend/Tests/course_tests.py
+++ Backend/Tests/course_tests.py
@@ -23,7 +23,7 @@ class TeacherCourseTests(unittest.TestCase):
         self.assertIn('TEST3', list(json_data.values())) 
         print("\n✅ Test - Successful Course Retrieval")
 
-    # FIX TEST    
+    # FIX TEST GET ALL
 
     def test_get_all_success(self):
         """Test successful retrieval of all courses by teacher_id."""
@@ -57,9 +57,7 @@ class TeacherCourseTests(unittest.TestCase):
         course_id = response.get_data(as_text=True)
         self.assertEqual(response.status_code, 200)
         self.assertTrue(ObjectId.is_valid(course_id))
-        
-        # I'll delete courses manually until the /add route returns a course_id as well!
-        
+                
         delete_response = self.client.delete('/course/teacher/delete', data={'course_id': course_id})
         
         self.assertEqual(delete_response.status_code, 200)
@@ -70,60 +68,93 @@ class TeacherCourseTests(unittest.TestCase):
 
     # Schema for new tests to be made
     
-    # def test_get_assignments(self):
-    #     """Test retrieving assignments for a course."""
-    #     course_id = '67e29e4c900069427cc4c358'
+    # ASSIGNMENT TESTS
+    def test_get_assignments(self):
+        """Test retrieving assignments for a course."""
+        course_id = '67e29e4c900069427cc4c358'
     
-    #     response = self.client.post('/course/teacher/get-assignments', data={'course_id': course_id})
+        response = self.client.post('/course/teacher/get-assignments', data={'course_id': course_id})
         
-    #     if response.status_code == 200:
-    #         json_data = response.get_json()
-    #         self.assertIsInstance(json_data, list)
-    #         print("\n✅ Test - Assignments Retrieved Successfully")
+        if response.status_code == 200:
+            json_data = response.get_json()
+            self.assertIsInstance(json_data, list)
+            print("\n✅ Test - Assignments Retrieved Successfully")
 
-    #     else:
-    #         self.fail(f"Unexpected response: {response.data.decode('utf-8')}")
+        else:
+            self.fail(f"Unexpected response: {response.data.decode('utf-8')}")
 
-    # def test_get_students(self):
-    #     """Test retrieving students for a course."""
-    #     course_id = '67e29e4c900069427cc4c358'
+    def test_add_assignment(self):
+        """Test adding an assignment for a course."""
+        course_id = '67e29e50900069427cc4c359'
+        title = 'TEST ASSIGNMENT'
+        date_start = '02.08.2003'
+        date_end = '02.08.2003'
+
+        response = self.client.post('/course/teacher/add-assignment', data={
+            'course_id': course_id,
+            'title': title,
+            'date_start': date_start,
+            'date_end': date_end
+        })
+
+        assignment_id = response.get_data(as_text=True)
+        self.assertEqual(response.status_code, 200)
+
+        delete_response = self.client.delete('/course/teacher/remove-assignment', data={
+            'course_id': course_id,
+            'assignment_id': assignment_id})
         
-    #     response = self.client.post('/teacher-course/get-students', data={'course_id': course_id})
-    #     self.assertIn(response.status_code, [200, 400])
-
-    #     if response.status_code == 200:
-    #         json_data = response.get_json()
-    #         self.assertIsInstance(json_data, list)
-    #         print("\n✅ Test - Students Retrieved Successfully")
-    #     else:
-    #         self.assertEqual(response.data.decode('utf-8'), 'No Student ID Provided')
-    #         print("\n✅ Test - No Students Found")
-
-    # def test_add_student(self):
-    #     """Test adding a student to a course."""
-    #     course_id = '67e29e4c900069427cc4c358'
+        self.assertEqual(delete_response.status_code, 200)
+        self.assertIn(f'Course with ID {course_id} deleted successfully', delete_response.get_data(as_text=True))
         
-    #     student_data = {
-    #         'first_name': 'John',
-    #         'last_name': 'Doe',
-    #         'course_id': course_id
-    #     }
+        print("\n✅ Test - Add and Delete Assignment Successful")
 
-    #     response = self.client.post('/teacher-course/add-student', data=student_data)
-    #     self.assertIn(response.status_code, [200, 404])
 
-    #     if response.status_code == 200:
-    #         student_id = response.get_data(as_text=True)
-    #         self.assertTrue(ObjectId.is_valid(student_id))
-    #         print("\n✅ Test - Student Successfully Added to Course")
+    # STUDENT TESTS
+    def test_get_students(self):
+        """Test retrieving students for a course."""
+        course_id = '67e29e4c900069427cc4c358'
+        
+        response = self.client.post('/course/teacher/get-students', data={'course_id': course_id})
+        self.assertIn(response.status_code, [200, 400])
 
-    #         # Remove student
-    #         remove_response = self.client.post('/teacher-course/remove-student', data={'course_id': course_id, 'student_id': student_id})
-    #         self.assertEqual(remove_response.status_code, 200)
-    #         self.assertEqual(remove_response.data.decode('utf-8'), 'Student Removed Successfully')
-    #     else:
-    #         self.assertEqual(response.data.decode('utf-8'), 'Student Not Found')
-    #         print("\n❎ Test - Student Not Found")
+        if response.status_code == 200:
+            json_data = response.get_json()
+            self.assertIsInstance(json_data, list)
+            print("\n✅ Test - Students Retrieved Successfully")
+        else:
+            self.assertEqual(response.data.decode('utf-8'), 'No Student ID Provided')
+            print("\n✅ Test - No Students Found")
+
+    def test_add_student(self):
+        """Test adding a student to a course."""
+        course_id = '67e29e4c900069427cc4c358'
+        
+        student_data = {
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'course_id': course_id
+        }
+
+        response = self.client.post('/course/teacher/add-student', data=student_data)
+        self.assertIn(response.status_code, [200, 404])
+
+        if response.status_code == 200:
+            student_id = response.get_data(as_text=True)
+            self.assertTrue(ObjectId.is_valid(student_id))
+            print("\n✅ Test - Student Successfully Added to Course")
+
+            # Remove student
+            remove_response = self.client.post('/course/teacher/remove-student', data={'course_id': course_id, 'student_id': student_id})
+            self.assertEqual(remove_response.status_code, 200)
+            self.assertEqual(remove_response.data.decode('utf-8'), 'Student Removed Successfully')
+        else:
+            self.assertEqual(response.data.decode('utf-8'), 'Student Not Found')
+            print("\n❎ Test - Student Not Found")
+
+    
+
+
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
@@ -131,9 +162,12 @@ if __name__ == '__main__':
     suite.addTest(TeacherCourseTests('test_get_one_success'))
     suite.addTest(TeacherCourseTests('test_get_all_success'))
     suite.addTest(TeacherCourseTests('test_add_course'))
-    # suite.addTest(TeacherCourseTests('test_get_assignments'))
-    # suite.addTest(TeacherCourseTests('test_get_students'))
-    # suite.addTest(TeacherCourseTests('test_add_student'))
+
+    suite.addTest(TeacherCourseTests('test_get_assignments'))
+    suite.addTest(TeacherCourseTests('test_add_assignment'))
+    
+    suite.addTest(TeacherCourseTests('test_get_students'))
+    suite.addTest(TeacherCourseTests('test_add_student'))
 
     runner = unittest.TextTestRunner()
     runner.run(suite)
