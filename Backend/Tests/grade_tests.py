10bbdf1108807101586ddcf8a289fadf9815c8a5
codralexandra <ioana.codreanu@student.unitbv.ro>
2025-04-01 17:59:28 +0300

    SCRUM-69 Test for bulk grade upload

--- Backend/Tests/grade_tests.py
+++ Backend/Tests/grade_tests.py
@@ -14,28 +14,39 @@ class GradeTests(unittest.TestCase):
         self.client.testing = True
     
     def test_add_grade_success(self):
-        student_ids = ['67e53c5960f8a5d53171ec46']
-        assignment_ids = ['67e7e56f3e25b983ea4a036f']
+        student_ids = ['67e53c3f60f8a5d53171ec45']
+        assignment_ids = ['67ebfbc7a97c28fd3846c0c6']
         scores = ['10']
         response = self.client.post('/grade/post', data={
-            'student_ids': '67e29e52900069427cc4c35a',
+            'student_ids': student_ids,
             'assignment_ids': assignment_ids,
             'scores': scores
         })
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.data)
 
-        # Finish when a delete route is added
-        # self.client.delete('/grade/delete', data={
-        #     'grade_id': 'miaumiau',
-        # })
+        print("\n✅ Test - Successful Grade Upload")
 
-        print("\n✅ Test - Successful Assignment Addition")
+    def test_add_bulk_success(self):
+        student_ids = ['67e53c3f60f8a5d53171ec45', '67e53bd260f8a5d53171ec44']
+        assignment_ids = ['67ebfbc7a97c28fd3846c0c6', '67ebfbc7a97c28fd3846c0c6']
+        scores = ['3', '3']
+        response = self.client.post('/grade/post', data={
+            'student_ids': student_ids,
+            'assignment_ids': assignment_ids,
+            'scores': scores
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.data)
+        
+        print("\n✅ Test - Successful Bulk Grade Upload")
+
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
     
     suite.addTest(GradeTests('test_add_grade_success'))
+    suite.addTest(GradeTests('test_add_bulk_success'))
 
     runner = unittest.TextTestRunner()
     runner.run(suite)
