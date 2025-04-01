ef3718845d987c3b75382da129f1a6d13b0221a3
ioana manghiuc <147768804+ioana-manghiuc@users.noreply.github.com>
2025-04-02 01:30:24 +0300

    SCRUM-86 Initial UI for Student.

--- frontend/src/Components/StudentPage.js
+++ frontend/src/Components/StudentPage.js
@@ -1,7 +1,45 @@
+import '../Stylesheets/HomePages.css';
+import { Course } from '../Models/Course';
+import { Student } from '../Models/Student';
+import { Assignment } from '../Models/Assignment';
+
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import axiosClient from '../API/axiosClient';
+
 function StudentPage() {
+  const location = useLocation();
+  const email = location.state?.email;
+  const student_id = location.state?.student_id;
+  const [selectedCourseId, setSelectedCourseId] = useState(null);
+
   return (
     <div>
-      <h1>Student Page</h1>
+       <div className="app-bar">{email}</div>
+
+       <div className="data-wrapper">
+         {/* COURSES */}
+          <div className="data-individual-wrapper">
+            <h2>Courses</h2>
+            <div className="data-container" style={{ position: 'static' }}>
+
+            </div>
+
+          </div>
+
+          {/* ASSIGNMENTS */}
+          {selectedCourseId && (
+            <div className="data-individual-wrapper">
+              <h2>Assignments</h2>
+              <div className="data-container" style={{ position: 'static' }}>
+
+              </div>
+
+            </div>
+          )}
+
+       </div>
+
     </div>
   );
 }
